56f8795d0f1c73f56b0a7b72cf67e63fd110be14
NanderSantos <nandermgsdp@gmail.com>
2021-01-15 11:10:04 -0300

    Criação da estrutura de diretórios e dos arquivos.

---
+++ source/MySTM32_IR_API.c
@@ -0,0 +1,11 @@
+/*
+ *  MySTM32_IR_API.c
+ *	Universidade Federal de Minas Gerais
+ *  Created on: Jan 2021
+ *  Author: Nander Carmo, Marcos Ribeiro 
+ *  Version: 1.0
+ *  License: MIT
+ */
+
+#include "MySTM32_IR_API.h"
+
---
+++ source/MySTM32_IR_API.h
@@ -0,0 +1,22 @@
+/*
+ *  MySTM32_IR_API.h
+ *	Universidade Federal de Minas Gerais
+ *  Created on: Jan 2021
+ *  Author: Nander Carmo, Marcos Ribeiro 
+ *  Version: 1.0
+ *  License: MIT
+ */
+
+#ifndef MY_STM32_IR_API_H
+#define MY_STM32_IR_API_H
+
+// Type definitions
+
+
+// Variable Definitions
+
+
+// Functions definitions
+
+
+#endif
